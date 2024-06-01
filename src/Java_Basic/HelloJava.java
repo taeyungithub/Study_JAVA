0a3461ca96ef41c6f321eee33466b2ea9a45b939
taeyungithub <rlaxodbs629@naver.com>
2024-06-01 12:48:59 +0900

    24-06-01(자바 복습 시작)

---
+++ src/Java_Basic/HelloJava.java
@@ -0,0 +1,8 @@
+package Java_Basic;
+
+public class HelloJava {
+//    프로그램 시작점
+    public static void main(String[] args){
+        System.out.println("hello java");
+    }
+}
---
+++ src/Java_Basic/HelloJava2.java
@@ -0,0 +1,10 @@
+package Java_Basic;
+
+public class HelloJava2 {
+    //    프로그램 시작점
+    public static void main(String[] args){
+        System.out.println("hello java");
+        System.out.println("hello java2");
+        System.out.println("hello java3");
+    }
+}
