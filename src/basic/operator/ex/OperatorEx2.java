c96ac316526a34b60291ba521bf7098a78ea96af
taeyungithub <rlaxodbs629@naver.com>
2024-06-01 21:00:03 +0900

    24-06-01(연산자)

---
+++ src/basic/operator/ex/OperatorEx2.java
@@ -0,0 +1,14 @@
+package basic.operator.ex;
+
+public class OperatorEx2 {
+    public static void main(String[] args) {
+        double var1 = 1.5;
+        double var2 = 2.5;
+        double var3 = 3.5;
+
+        double sum = var1 + var2 + var3;
+        double average = sum / 3;
+
+        System.out.println("sum = " + sum + ", average = " + average);
+    }
+}
---
+++ src/basic/operator/ex/OperatorEx3.java
@@ -0,0 +1,10 @@
+package basic.operator.ex;
+
+public class OperatorEx3 {
+    public static void main(String[] args) {
+        int score = 90;
+        boolean result = score >= 80 && score <= 100;
+
+        System.out.println(result);
+    }
+}
